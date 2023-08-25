ecc0f7e9c23b5788b6ac38c4549e0d3663588fb9
tendai-gatahwa <107326659+tendai-gatahwa@users.noreply.github.com>
2023-08-25 15:34:43 +0200

    Update dalle.routes.js

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import OpenAI from 'openai';
+import { Configuration, OpenAIApi} from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
+
+const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" })
@@ -20,7 +20,7 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const response = await openai.images.generate({
+    const response = await openai.createImage({
       prompt,
       n: 1,
       size: '1024x1024',
